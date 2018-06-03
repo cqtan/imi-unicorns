588bae7985dfc0e465db2c8abd88dcb54a3cc72d
CouchCat <s0547139@htw-berlin.de>
2018-06-04 01:11:34 +0200

    Add replaced opvencv methods with PIL.  Add logging and conversion to jpeg.

--- classify-all.py
+++ classify-all.py
@@ -1,37 +1,47 @@
-# USAGE
-# python classify-all.py --model filter_v2.model --labelbin label_v2.pickle --images data
-
-# import the necessary packages
 from keras.preprocessing.image import img_to_array
 from keras.models import load_model
 from imutils import paths
 import numpy as np
 #import tensorflow as tf
 import argparse
-import imutils
 import pickle
-import cv2
+from PIL import Image
 import os
+import logging
+from time import gmtime, strftime
+from datetime import datetime
 
-""" # Configure amount of GPU memory in % to allocate:
-config = tf.GPUOptions(per_process_gpu_memory_fraction=0.870)
-sess = tf.Session(config=tf.ConfigProto(gpu_options=config)) """
+'''
+Reads all images listed in the 'data_path' variable and classifies images according to
+the classes in listed in the label.pickle file. Currently:
+- blanks
+- color_palette
+- content
+- logo
+- red_stamp
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-m", "--model", required=True,
-	help="path to trained model model")
-ap.add_argument("-l", "--labelbin", required=True,
-	help="path to label binarizer")
-ap.add_argument("-i", "--images", required=True,
-	help="path to input images")
-args = vars(ap.parse_args())
+Additionally, classifies images as 'other' if image is less than 85% classified as 'content'.
+Also converts all images from TIFF to JPEG and saves them in the 'output' directory with 
+their respective class and PPN.
+'''
+
+# Configure amount of GPU memory in % to allocate:
+# config = tf.GPUOptions(per_process_gpu_memory_fraction=0.870)
+# sess = tf.Session(config=tf.ConfigProto(gpu_options=config))
+
+# Configure logging
+current_time = strftime("%Y-%m-%d_%H-%M-%S", gmtime())
+logging.basicConfig(filename='LOG_' + current_time + '.log',level=logging.DEBUG)
+
+model_path = 'filter_v2.model'
+labelbin_path = 'label_v2.pickle'
+data_path = 'data'
 
 # load the trained convolutional neural network and the label
 # binarizer
 print("[INFO] loading network...")
-model = load_model(args["model"])
-lb = pickle.loads(open(args["labelbin"], "rb").read())
+model = load_model(model_path)
+lb = pickle.loads(open(labelbin_path, "rb").read())
 
 # Create output directory and subdirectories
 if not os.path.exists('output'):
@@ -40,29 +50,48 @@ if not os.path.exists('output'):
 		os.makedirs('output/'+cl)
 
 # loop over all images in each subdirectory
-imagePaths = sorted(list(paths.list_images(args["images"])))
+print("[INFO] classifying images...")
+imagePaths = sorted(list(paths.list_images(data_path)))
+logging.info("Logging latest PPN classified...")
+last_ppn = ''
 for imagePath in imagePaths:
 	# load and save a copy of the image
-	image = cv2.imread(imagePath)
+	image = Image.open(imagePath)
 	output = image.copy()
-	ppn = imagePath.split(os.path.sep)[-2]
+
+	# Store image info
 	filename = imagePath.split(os.path.sep)[-1]
+	filename = filename[:-3]
+	ppn = imagePath.split(os.path.sep)[-2]
+	if last_ppn != ppn:
+		last_ppn = ppn
+		logging.info(str(datetime.now()) + ' ' + ppn)
 
 	# pre-process the image for classification
-	image = cv2.resize(image, (96, 96))
-	image = image.astype("float") / 255.0
-	image = img_to_array(image)
+	image = image.resize((96, 96))
+	image = np.array(image)
+	image = np.divide(image, 255.0)
 	image = np.expand_dims(image, axis=0)
 
 	# classify the input image
-	print("[INFO] classifying image...")
 	proba = model.predict(image)[0]
 	idx = np.argmax(proba)
 	label = lb.classes_[idx]
+	if label == 'content' and (proba[np.argsort(proba)[-1:]]) < 0.85:
+		label = 'other'
+		print('[INFO] Check unidentified content in others directory: ', ppn + ', ' + filename)
+		logging.info('Check unidentified content in others directory: ' + ppn + ', ' + filename)
 
 	# within the predicted subdirectory of the class create
 	# another subdirectory according to the PPN of the image.
 	out_path = 'output/'+label+'/'+ppn
 	if not os.path.exists(out_path):
 		os.makedirs(out_path)
-	cv2.imwrite(os.path.join(out_path,filename), output)
+	output.save(os.path.join(out_path,filename)+'jpg')
+
+	# Log latest PPN
+	if last_ppn != ppn:
+		last_ppn = ppn
+		logging.info(datetime.now() + ' ' + ppn)
+
+print("[INFO] Done! Output directory created!")
