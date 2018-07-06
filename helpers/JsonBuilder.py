a0b3c78e292f99afe5d43cf437b3f11c15ac79b9
ctan <s0547139@htw-berlin.de>
2018-07-06 11:14:00 +0200

    Add alternative json with feature accuracies in categories_alt.json

--- helpers/JsonBuilder.py
+++ helpers/JsonBuilder.py
@@ -1,12 +1,13 @@
 import json
 import numpy as np
 import os
-
+import math
 
 class JsonBuilder:
 
     def __init__(self, labels, threshold):
         self.output_filename = "categories.json"
+        self.output_filename_alt = "categories_alt.json"
         self.labels = labels
         self.threshold = threshold
         self.master_dict = {
@@ -26,8 +27,14 @@ class JsonBuilder:
             json.dump(self.master_dict, json_file, indent=4)
         print("Created " + self.output_filename)
 
+        with open(self.output_filename_alt, 'w') as json_file:
+            json.dump(self.alt_dict, json_file, indent=4)
+        print("Created " + self.output_filename_alt)
+        
+
     def AppendImageData(self, image_path, predictions):
         features = self.GetFeatures(predictions)
+        features_with_acc = self.GetFeaturesWithAcc(predictions)
         path = self.BuildCustomPath(image_path)
         ppn = image_path.split(os.path.sep)[-2]
 
@@ -38,7 +45,13 @@ class JsonBuilder:
         image_dict['path'] = path
         image_dict['ppn'] = ppn
 
+        image_dict_acc = {}
+        image_dict_acc['features'] = features_with_acc
+        image_dict_acc['path'] = path
+        image_dict_acc['ppn'] = ppn
+
         self.master_dict['image_data'].append(image_dict)
+        self.alt_dict['image_data'].append(image_dict_acc)
 
     def AppendBookData(self, ppn, features):
         if ppn not in self.master_dict["book_data"]:
@@ -47,16 +60,24 @@ class JsonBuilder:
         for feature in features:
             if feature not in self.master_dict["book_data"][ppn]:
                 self.master_dict["book_data"][ppn].append(feature)
-        
 
+        if ppn not in self.alt_dict["book_data"]:
+            self.alt_dict["book_data"][ppn] = []
+
+        for feature in features:
+            if feature not in self.alt_dict["book_data"][ppn]:
+                self.alt_dict["book_data"][ppn].append(feature)
+        
     def AppendCategoryData(self):
         for label in self.labels:
             title = label.title()
             self.master_dict['category_data'][title] = label
+            self.alt_dict['category_data'][title] = label
 
     def GetFeatures(self, predictions):
         features = []
         pred_list = predictions.tolist()
+
         for preds in pred_list:
             for pred in preds:
                 if pred > self.threshold:
@@ -64,7 +85,18 @@ class JsonBuilder:
                     features.append(self.labels[idx])
         return features
 
-    # dist/ChasingUnicornsAndVampires/assets/images/" + PPN + <imageName.ending>
+    def GetFeaturesWithAcc(self, predictions):
+        features = {}
+        pred_list = predictions.tolist()
+        highest_pred = math.floor(predictions.max() * 100)
+
+        for preds in pred_list:
+            for pred in preds:
+                if pred > self.threshold:
+                    idx = np.argmax(predictions)
+                    features[self.labels[idx]] = highest_pred
+        return features
+
     def BuildCustomPath(self, image_path):
         custom_path = "dist/ChasingUnicornsAndVampires/assets/images"
         ppn = image_path.split(os.path.sep)[-2]
