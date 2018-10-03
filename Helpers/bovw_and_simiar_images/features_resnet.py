c56c193b3080aea28a512b3fa72bff9cf3bda8fc
LuiRa <LuiRa@users.noreply.github.com>
2018-10-03 18:01:09 +0200

    changed cluster numbers

--- Helpers/bovw_and_simiar_images/features_resnet.py
+++ Helpers/bovw_and_simiar_images/features_resnet.py
@@ -39,7 +39,7 @@ images = []
 bag = []
 descriptors = []
 
-feat_cluster_nums = [800]
+feat_cluster_nums = [1000,1500,2000,2500,3000,3500,4000,4500,5000,5500,6000,6500,7000,7500,8000,8500,9000,9500,10000]
 dist = DistanceMetric.get_metric('euclidean')
 
 
